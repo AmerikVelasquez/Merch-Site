241d1c74d4416311f5b4579ceb2573a151a357bd
Shaun Kent <shaunkent81@gmail.com>
2022-01-10 20:57:04 -0800

    create merch control component

---
+++ src/components/MerchList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Merch from './Merch';
+import PropTypes from "prop-types";
+
+
+
+function MerchList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.merchList.map((merch) => 
+        <Merch 
+          name={merch.name}
+          description={merch.description}
+          quantity={merch.quantity}
+          id = {merch.id}
+          key={merch.id}
+          />
+      )}
+    </React.Fragment>
+  );
+}
+
+MerchList.propTypes = {
+  merchList: PropTypes.array,
+  onMerchSelection: PropTypes.func
+};
+
+export default MerchList;
