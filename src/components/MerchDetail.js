99257a188ebebb8cebb68604965b0d68bea234d8
Shaun Kent <shaunkent81@gmail.com>
2022-01-11 19:58:33 -0800

    add delte and edit buttons and connects them to merchControl file

--- src/components/MerchDetail.js
+++ src/components/MerchDetail.js
@@ -3,19 +3,24 @@ import PropTypes from 'prop-types'
 
 
 function MerchDetail(props) {
-  const { merch } = props;
+  const { merch, onClickingDelete } = props;
   return (
     <React.Fragment>
       <h1>Merch Detail</h1>
       <h3>{merch.name}</h3>
       <p>{merch.description}</p>
       <p>Quantity: {merch.quantity}</p>
+      <button onClick = {props.onClickingEdit}>Update Merch</button>
+      <button onClick = {()=> onClickingDelete(merch.id)}> Close Merch</button>
+
     </React.Fragment>
   );
 }
 
 MerchDetail.propTypes = {
-  merch: PropTypes.object
+  merch: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default MerchDetail;
